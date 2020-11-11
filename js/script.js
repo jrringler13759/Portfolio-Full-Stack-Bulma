26df5f49a9918dafc13611fac01e91c2608d0c73
Nikita <jrringler13759@gmail.com>
2020-11-11 16:49:55 -0500

    redoing style sheet

--- js/script.js
+++ js/script.js
@@ -14,6 +14,18 @@
 // }
 
 
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
+
 
 function myFunction() {
     var x = document.getElementById("navItems");
@@ -27,28 +39,28 @@ function myFunction() {
 
 
 
-  function loadImade (id, targetID) {
-    var el = document.getElementById(id);
-    var targetEl = targetID ? document.getElementById(targetID) : el;
-    var imageToLoad;
+//   function loadImade (id, targetID) {
+//     var el = document.getElementById(id);
+//     var targetEl = targetID ? document.getElementById(targetID) : el;
+//     var imageToLoad;
 
-    if (el.dataset.image) {
-        imageToLoad = el.dataset.image;
-    } else if (typeof el.currentSrc === 'undefined') {
-        imagetTOLoad = el.scroll;
-    } else {
-        imageToLoad = currentSrc;
-    }
+//     if (el.dataset.image) {
+//         imageToLoad = el.dataset.image;
+//     } else if (typeof el.currentSrc === 'undefined') {
+//         imagetTOLoad = el.scroll;
+//     } else {
+//         imageToLoad = currentSrc;
+//     }
 
-    if (imageToLoad) {
-        var img = new Image();
-        img.src = imageToLoad;
-        img.onload = function() {
-            targetEl.classList.add('is-loaded');
-        }
-    }
+//     if (imageToLoad) {
+//         var img = new Image();
+//         img.src = imageToLoad;
+//         img.onload = function() {
+//             targetEl.classList.add('is-loaded');
+//         }
+//     }
 
-}
+// }
 
 
 document.addEventListener('DOMContentLoaded', function() { 
